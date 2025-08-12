34c0ee2ae4c5a0b643bdec3446c9ba41c98fe044
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-08-12 17:44:49 +0100

    Fix country name retrieval in users table view

--- resources/views/layouts/seven/users/table.blade.php
+++ resources/views/layouts/seven/users/table.blade.php
@@ -23,7 +23,7 @@
                     </a>
                     @if (filled($user->country))
                         <span class="fi fi-{{ $user->country }} ml-2"
-                            title="{{ $country->alpha2($user->country)['name'] }}"></span>
+                            title="{{ $country[($user->country)]['name'] }}"></span>
                     @endif
                 </td>
                 <td>{{ $user->airline->icao }}</td>
